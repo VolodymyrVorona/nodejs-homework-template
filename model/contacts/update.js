a7ad6a306a8deaa31ea8b6b227e191b73ff0fd40
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-08-24 10:46:10 +0300

    Adds functions for working with contacts.json file

---
+++ model/contacts/update.js
@@ -0,0 +1,21 @@
+const updateContactsFile = require("./updateContactsFile");
+const getAll = require("./getAll");
+
+const update = async (id, updateInfo) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const contacts = await getAll();
+    const idx = contacts.findIndex((item) => item.id === id);
+    if (idx === -1) {
+      return null;
+    }
+    contacts[idx] = { ...contacts[idx], ...updateInfo };
+
+    await updateContactsFile(contacts);
+    return contacts[idx];
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = update;
